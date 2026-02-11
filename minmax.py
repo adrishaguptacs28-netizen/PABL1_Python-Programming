1c8b26ab105dae51ce859bf4c26f6aa8cef86f56
adrisha33 <guptaadrisha@gmail.com>
2026-02-11 10:19:10 +0530

    Create minmax.py

---
+++ minmax.py
@@ -0,0 +1,12 @@
+arr = [1, 4, 3, 5, 8, 6]
+
+minimum = arr[0]
+maximum = arr[0]
+
+for num in arr:
+    if num < minimum:
+        minimum = num
+    if num > maximum:
+        maximum = num
+
+print([minimum, maximum])
