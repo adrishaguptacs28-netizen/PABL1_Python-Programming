a5273952c19a2a3d7a7af88ee813f13d622a7a6a
Adrisha Gupta <adrisha.gupta.cs28@iilm.edu>
2026-04-18 02:35:31 +0530

    Create rowwithmax.py

---
+++ rowwithmax.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rowWithMax1s(self, arr):
+        n = len(arr)
+        m = len(arr[0])
+        
+        row = 0
+        col = m - 1
+        
+        max_row = -1
+        
+        while row < n and col >= 0:
+            if arr[row][col] == 1:
+                max_row = row
+                col -= 1  
+            else:
+                row += 1   
+        
+        return max_row
